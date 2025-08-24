3410f1d6dbd4ce1428b89a6eb73b7924d24972c7
Roma Galyh <romagalyh@gmail.com>
2025-08-24 20:48:18 +0300

    add recipes section

---
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,32 @@
+import style from "./RecipeCard.module.css"
+import Icon from "../../shared/Icon"
+
+
+export function RecipeCard({ dishPhoto, recipeName, recipeDescription, recipeTime }) {
+    
+    return (
+        <div className={style.card}>
+
+            <img className={style.img} src={dishPhoto} alt={recipeName} />
+           
+            <div className={style.header}>
+                <h3 className={style.title}> {recipeName} </h3>
+                <div className={style.containerTime}>
+                    <p className={style.time}> <Icon name="clock" classname={style.icon} /> {recipeTime} </p>
+                </div>
+                
+            </div>
+            
+            <div className={style.descrContainer}>
+                <p className={style.description}> {recipeDescription} </p>
+                <p className={style.cals}> - cals </p>
+            </div>
+
+            <div className={style.btnContainer}>
+                <button className={style.btn}> Learn more </button>
+                <button className={style.btnIcon}>  <Icon name="flag" classname={style.icon} /> </button>
+            </div>
+        </div>
+    )
+
+};
