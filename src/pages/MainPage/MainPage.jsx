56fabd53d4200cab0f6901cb90b1f1fdc6b1e436
MazaFaaka <haitovvasyl@gmail.com>
2025-08-25 02:15:58 +0200

    delete path userInfo mainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,10 @@
-
 import RecipesSection from "../../components/RecipesSection/RecipesSection.jsx";
-import UserInfo from "../../components/UserInfo/UserInfo";
 export default function MainPage() {
   return (
     <>
       <h2>I main page</h2>
 
-      <RecipesSection/>
-      <UserInfo/>
+      <RecipesSection />
     </>
   );
 }
