fe9e4bc844c9df1b36a38ff380e68e6a8cfb110b
Vasyl <workjobsproject@gmail.com>
2025-08-28 17:42:28 +0200

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -31,7 +31,7 @@ export const fetchLoginUser = createAsyncThunk(
   async (credentials, thunkAPI) => {
     console.log("THUNK STARTED", credentials);
     try {
-      const res = await apiClient.post("/auth/login", credentials);
+      const res = await apiClient.post("/api/auth/login", credentials);
       console.log("LOGIN RESPONSE:", res);
 
       const { accessToken } = res.data.data;
