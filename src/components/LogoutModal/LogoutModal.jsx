91749bb961774a8fe6f4c5d0c6019a478cdefff4
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-08-28 14:30:17 +0300

    updated publicApi

--- src/components/LogoutModal/LogoutModal.jsx
+++ src/components/LogoutModal/LogoutModal.jsx
@@ -5,26 +5,28 @@ import css from "./LogoutModal.module.css";
 export default function LogoutModal({ onConfirm, onCancel }) {
   return (
     <Modal title="Are you sure?" message="We will miss you!" onClose={onCancel}>
-      <Button
-        type="button"
-        styleType="read"
-        name="logout"
-        paddingsY="12"
-        paddingsX="54"
-        aria-label="Log out"
-        className={css.logoutBtn}
-        onClick={onConfirm}
-      />
-      <Button
-        type="button"
-        styleType="transparent"
-        name="cancel"
-        paddingsY="12"
-        paddingsX="54"
-        aria-label="Cancel"
-        className={css.logoutBtn}
-        onClick={onCancel}
-      />
+      <div className={css.btnWrapper}>
+        <Button
+          type="button"
+          styleType="read"
+          name="Log out"
+          paddingsY="12"
+          paddingsX="54"
+          aria-label="Log out"
+          className={css.logoutBtn}
+          onClick={onConfirm}
+        />
+        <Button
+          type="button"
+          styleType="transparent"
+          name="cancel"
+          paddingsY="12"
+          paddingsX="54"
+          aria-label="Cancel"
+          className={css.logoutBtn}
+          onClick={onCancel}
+        />
+      </div>
     </Modal>
   );
 }
