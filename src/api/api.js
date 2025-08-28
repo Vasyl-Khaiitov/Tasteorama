bb4bf2833aaa5f2ef51a8a2c6da9a00bfb59ca32
Vasyl <workjobsproject@gmail.com>
2025-08-28 17:49:23 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,6 @@ import { lsRemoveToken, lsSetToken } from "../utils/localStorage";
 
 const apiClient = axios.create({
   baseURL: "https://cook-api-dtdl.onrender.com/api",
-  withCredentials: true,
 });
 
 export const setAuthorizationToken = (token) => {
