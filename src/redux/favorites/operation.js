f081c398df06f3527b56b5a69894728517e675b0
Vladimir <kremench@gmail.com>
2025-08-29 18:29:39 +0300

    Finally fix favorites section

--- src/redux/favorites/operation.js
+++ src/redux/favorites/operation.js
@@ -5,7 +5,6 @@ export const fetchFavoriteRecipes = createAsyncThunk(
   "recipes/fetchFavoritesRecipes",
   async ({ userId, token, page, perPage }, thunkAPI) => {
     try {
-      
       const res = await apiClient.get(`/recipes/${userId}/favorites`, {
         params: {
           page,
@@ -16,7 +15,6 @@ export const fetchFavoriteRecipes = createAsyncThunk(
           Authorization: `Bearer ${token}`,
         },
       });
-      console.log("FETCH FAVORITES RESPONSE:", res.data);
       return res.data;
     } catch (err) {
       return thunkAPI.rejectWithValue(err.message);
@@ -24,32 +22,24 @@ export const fetchFavoriteRecipes = createAsyncThunk(
   }
 );
 export const addToFavorites = createAsyncThunk(
-    "favorites/add",
-    async(recipeId,{rejectWithValue})=>{
-      try{
-        await apiClient.post(`/recipes/${recipeId}/favorite`);
-        return recipeId;
-      }
-      catch(err){
-        return rejectWithValue(err.response?.data || err.message);
-      }
+  "favorites/add",
+  async (recipeId, { rejectWithValue }) => {
+    try {
+      await apiClient.post(`/recipes/${recipeId}/favorite`);
+      return recipeId;
+    } catch (err) {
+      return rejectWithValue(err.response?.data || err.message);
     }
-
-
-
+  }
 );
 export const deleteFromFavorites = createAsyncThunk(
-    "favorites/delete",
-    async(recipeId,{rejectWithValue})=>{
-      try{
-        await apiClient.delete(`/recipes/${recipeId}/favorite`);
-        return recipeId;
-      }
-      catch(err){
-        return rejectWithValue(err.response?.data || err.message);
-      }
+  "favorites/delete",
+  async (recipeId, { rejectWithValue }) => {
+    try {
+      await apiClient.delete(`/recipes/${recipeId}/favorite`);
+      return recipeId;
+    } catch (err) {
+      return rejectWithValue(err.response?.data || err.message);
     }
-
-
-
+  }
 );
